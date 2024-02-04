8d8b515d9c514593cd0cd4055e8d53ca2869f302
hFurkan <hasanfurkanbaltaci2@gmail.com>
2024-02-04 23:26:00 +0300

    Navbar

--- HotelProject/src/Components/CustomNavbar.jsx
+++ HotelProject/src/Components/CustomNavbar.jsx
@@ -5,30 +5,27 @@ import NavDropdown from "react-bootstrap/NavDropdown";
 
 function CustomNavbar() {
   return (
-    <Navbar
-      expand="lg"
-      className="bg-body-tertiary"
-      bg="dark"
-      data-bs-theme="dark"
-    >
+    <Navbar expand="lg" className="navbar">
       <Container>
-        <Navbar.Brand href="#home">Dedeman Otel</Navbar.Brand>
+        <Navbar.Brand href="#home">Dedeman Hotel</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+            <Nav.Link href="#home">HOME</Nav.Link>
+            <Nav.Link href="#services">SERVICES</Nav.Link>
+            <Nav.Link href="#rooms">ROOMS</Nav.Link>
+            <Nav.Link href="#booking">BOOKING</Nav.Link>
+            <NavDropdown
+              title="ABOUT US"
+              id="basic-nav-dropdown"
+              className="navbar-dropdown"
+            >
+              <NavDropdown.Item href="#action/3.1">Our Team</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
+                Testimonials
               </NavDropdown.Item>
             </NavDropdown>
+            <Nav.Link href="#link">CONTACT</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
