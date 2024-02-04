3152f585eec530cbd8e80341f6b6071180875f30
hFurkan <hasanfurkanbaltaci2@gmail.com>
2024-02-04 22:32:00 +0300

    Initial Commit

---
+++ HotelProject/src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import Button from "react-bootstrap/Button";
+function App() {
+  return (
+    <>
+      <Button as="a" variant="primary">
+        Button as link
+      </Button>
+      <Button as="a" variant="success">
+        Button as link
+      </Button>
+    </>
+  );
+}
+
+export default App;
